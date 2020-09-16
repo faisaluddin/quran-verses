3834076a4651c244e382e99a76d72dc80c4e6c0a
Rizwan <rizwanshoeb20596@gmail.com>
2020-09-16 16:22:06 +0530

    added url for contribute and added feedback page

--- src/read/urls.py
+++ src/read/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from django.views.generic import TemplateView
 from .views import HomePage, subscribe
 
+
 urlpatterns = [
     path('', HomePage.as_view(), name='home'),
     path('subscribe/', subscribe, name='subscribe'),
+    path('contribute/', TemplateView.as_view(template_name="read/contribute.html"),name='contribute'),
+     path('feedback/', TemplateView.as_view(template_name="read/feedback.html"),name='feedback'),
 ]
